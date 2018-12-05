d34b1a8bd968e8c2772b41c35bbc16eaaf430b68
masterabram <abramogol@gmail.com>
2018-12-05 15:59:58 +0300

    fixing raltive imports

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -3,9 +3,9 @@ from flask import Blueprint
 
 version_two = Blueprint('api_v2', __name__, url_prefix='/api/v2')
 
-from views.auth_views import api as auth_ns
-from views.incident_views import api as incidence_ns
-from views.comment_views import api as comment_ns
+from .views.auth_views import api as auth_ns
+from .views.incident_views import api as incidence_ns
+from .views.comment_views import api as comment_ns
 
 api = Api(version_two,
           title='Ireporter',
